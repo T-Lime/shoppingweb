e0068459630f8d71e55ac540299302311463d8e9
티라임 <lime_t@tilaim-ui-MacBookPro.local>
2026-05-26 13:38:20 +0900

    first commit

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,16 @@
+import { Outlet, Link } from "react-router";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+  return (
+    <div className={styles.container}>
+      <Link to="/">
+        <h1>다운몰</h1>
+      </Link>
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
